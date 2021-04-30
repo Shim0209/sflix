778058a36e5362294333005259bc94f4694d2bad
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2021-04-30 21:08:34 +0900

    Create sflix project

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import Router from "Components/Router";
+import Header from "Components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Router />
+    </>
+  )
+};
+export default App;
