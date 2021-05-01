c46913e6ba8f87c00a409e286807cdd10daa482a
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2021-05-02 01:33:25 +0900

    Add styled-component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,12 @@
+import React, { component } from "react";
 import Router from "Components/Router";
-import Header from "Components/Header";
+import GlobalStyles from "Components/GlobalStyles";
 
 function App() {
   return (
     <>
-      <Header />
       <Router />
+      <GlobalStyles />
     </>
   )
 };
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,14 +3,19 @@ import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
+import Header from "Components/Header";
 
 export default () => (
+    
     <Router>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} />
-            <Route path="/search"  component={Search} />
-            <Redirect from="*" to="/" />
-        </Switch>
+        <>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" exact component={TV} />
+                <Route path="/search"  component={Search} />
+                <Redirect from="*" to="/" />
+            </Switch>
+        </>
     </Router>
 )
